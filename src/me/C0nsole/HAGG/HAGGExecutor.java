684710adbadfc54d595e08f22ae987b28719a909
c0nsole <garrett2smart@gmail.com>
2013-05-15 18:46:17 -0500

    Created args
    I just… made the args...

--- src/me/C0nsole/HAGG/HAGGExecutor.java
+++ src/me/C0nsole/HAGG/HAGGExecutor.java
@@ -1,8 +1,10 @@
 package me.C0nsole.HAGG;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class HAGGExecutor implements CommandExecutor {
 	
@@ -14,12 +16,56 @@ public class HAGGExecutor implements CommandExecutor {
 	
 	@Override
 	   public boolean onCommand(CommandSender sender, Command cmd, String commandlabel, String [] args){
+		Player player = (Player) sender;
 		if(commandlabel.equalsIgnoreCase("hagg")){
-			
+			if(player.hasPermission("hagg.user")){
+				if(args.length == 1){
+					if(args[0].equalsIgnoreCase("join")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("leave")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("help")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("spec")){
+						
+					}
+					else{
+						player.sendMessage(ChatColor.RED + "Incorrect Syntax. Please do " + ChatColor.GREEN + "/hagg help");
+					}
+				}
+			}
 		}
 		if(commandlabel.equalsIgnoreCase("haggadmin")){
-			
+			if(player.hasPermission("hagg.admin")){
+				if(args.length == 1){
+					if(args[0].equalsIgnoreCase("createarena")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("delarena")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("setlobbyspawn")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("setplayerspawn")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("setgortumespawn")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("forcestart")){
+						
+					}
+					else if(args[0].equalsIgnoreCase("forcestop")){
+						
+					}
+				}
+			}
 		}
 		return false;
 	}
 }
+
